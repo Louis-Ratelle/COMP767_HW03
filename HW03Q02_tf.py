68f4d9a79f4b62bbacc4ae4b16cf8d06a48d622c
Michael Benitah <michael.benitah@umontreal.ca>
2020-04-09 10:01:51 -0400

    final commit

--- HW03Q02_tf.py
+++ HW03Q02_tf.py
@@ -5,25 +5,24 @@ import argparse
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import pickle
+# import a2c.py
 from datetime import datetime
 
 SEED = None
-GAMMA = 0.9
+GAMMA = 0.9 #LFPR: avant 0.9
 ALPHAS = 2.0**np.array([-6, -4, -2, 0])
 THETA_SIZE = 32
 W_SIZE = 32
 RUNS = 5
-EPISODES = 2000
+EPISODES = 2001 # LFPR: 2001 vraie valeur
 MAX_STEPS = 200
-UPDATE_EVERY = 10
+UPDATE_EVERY = 10 #LFPR
 ENV = 'CartPole-v1'
 SAVED_MODELS_FOLDER = './data/'
 NOW = "{0:%Y-%m-%dT%H-%M-%S}".format(datetime.now())
 
-n_episodes = 2000
-env = gym.make('CartPole-v1').unwrapped
-scores = []
-update_every = 10
+global best_scores_per_hyperparams
+best_scores_per_hyperparams = {}
 
 # #############################################################################
 #
@@ -94,7 +93,8 @@ def discount_rewards(r, gamma = 0.9):
     running_add = 0
     for t in reversed(range(0, r.size)):
         running_add = running_add * gamma + r[t]
-        discounted_r[t] = running_add
+        discounted_r[t] = running_add #LFPR
+        #discounted_r[t] = running_add * gamma**t
     return discounted_r
 
 
@@ -105,7 +105,7 @@ def discount_rewards(r, gamma = 0.9):
 # #############################################################################
 
 class policy(tf.keras.Model):
-    def __init__(self, env, seed=None):
+    def __init__(self, env, alpha, seed=None):
         super(policy, self).__init__()
 
         # sets the environment
@@ -114,87 +114,143 @@ class policy(tf.keras.Model):
 
         # build 2-layer MLP model
         self.model = tf.keras.Sequential()
+        #limit1 = (6 / (env.observation_space.shape[0] + args.theta_size))**0.5
+        #layer_init_1 = tf.keras.initializers.RandomUniform(minval=-limit1, maxval=limit1, seed=None)
         self.model.add(
             tf.keras.layers.Dense(args.theta_size,
                                   input_dim=env.observation_space.shape[0],
-                                  activation='relu'))
+                                  activation='relu')) # kernel_initializer=layer_init_1,
+
+        #limit2 = (6 / (args.theta_size+env.action_space.n))**0.5
+        #layer_init_2 = tf.keras.initializers.RandomUniform(minval=-limit2, maxval=limit2, seed=None)
         self.model.add(
-            tf.keras.layers.Dense(env.action_space.n,
-                                  activation='softmax'))
+            #tf.keras.layers.Dense(env.action_space.n,
+            #                      activation='softmax')) # LFPR
+            tf.keras.layers.Dense(env.action_space.n)) # kernel_initializer=layer_init_2
         self.model.build()
 
+        #self.optimizer = tf.keras.optimizers.Adam(learning_rate=alpha) # LFPR
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=alpha)
+        #self.optimizer = tf.keras.optimizers.RMSprop(learning_rate=alpha)
+        #self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.02)
+        #self.optimizer = tf.keras.optimizers.Adam(learning_rate=alpha, beta_1=0.8, beta_2=0.99)
+        #self.optimizer = tf.keras.optimizers.SGD(learning_rate=0.0005, momentum = 0.5) #learning_rate=0.001
+
         # initialize model parameters
-        self.theta = self.model.trainable_variables
-        for ix, grad in enumerate(self.theta):
-            self.theta[ix] = grad * 0
+        self.gradients = self.model.trainable_variables
+        for ix, grad in enumerate(self.gradients):
+            self.gradients[ix] = grad * 0
 
     def call(self, state):
+        state = state[None] # LFPR: pourquoi None ici ?
+        state = tf.convert_to_tensor(state) #LFPR: J'ai ajouté ça
         return self.model(state)
 
     def choose_action(self, state):
         'Returns action and gradients.'
 
         compute_loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        #compute_loss = tf.keras.losses.SparseCategoricalCrossentropy() # LFPR
 
-        state = state[None]
         with tf.GradientTape() as tape:
             # forward pass
             logits = self(state)
-            action_probs = logits.numpy()
+            #action_probs = logits.numpy()
+            action_probs = tf.nn.softmax(logits).numpy()
+            #print("action_probs: ", action_probs)
             # Choose random action with p = action dist
-            action = np.random.choice(action_probs[0], p=action_probs[0])
+            action = np.random.choice(action_probs[0], p=action_probs[0]) # LFPR
+            # Pourquoi ne pas utiliser list(range(len(action_probs[0]))), donc pas l'autre ligne
             action = np.argmax(action_probs == action)
             loss = compute_loss([action], logits)
         grads = tape.gradient(loss, self.model.trainable_variables)
 
         return action, grads
 
+    def update(self, episode):
+        for grads, r in episode:
+            for idx, grad in enumerate(grads):
+                self.gradients[idx] += grad * r
+
+    def update_actor(self, grads, I, delta):
+        for idx, grad in enumerate(grads):
+            self.gradients[idx] += I * delta * grad
+
+    def apply_gradients(self):
+        self.optimizer.apply_gradients(zip(self.gradients, self.model.trainable_variables))
+        # for idx, grad in enumerate(self.model.trainable_variables):
+        #     print('Sum theta_{}: {}'.format(idx, np.sum(np.abs(grad))))
+        for idx, grad in enumerate(self.gradients):
+            self.gradients[idx] = grad * 0
+
+
 
 class v_hat(tf.keras.Model):
-    def __init__(self, alpha_w):
-        super(policy, self).__init__()
+    def __init__(self, alpha_w, input_dim):
+        super(v_hat, self).__init__()
 
         # build 2-layer MLP model
         self.model = tf.keras.Sequential()
+        #limit1 = (6 / (input_dim + args.w_size)) ** 0.5
+        #layer_init_1 = tf.keras.initializers.RandomUniform(minval=-limit1, maxval=limit1, seed=None)
         self.model.add(
             tf.keras.layers.Dense(
                 args.w_size,
-                input_dim=env.observation_space.shape[0],
-                activation='relu'
-                )
+                input_dim=input_dim,
+                activation='relu',
+                ) # kernel_initializer=layer_init_1,
             )
+        #limit2 = (6 / (args.w_size + 1)) ** 0.5
+        #layer_init_2 = tf.keras.initializers.RandomUniform(minval=-limit2, maxval=limit2, seed=None)
+        self.model.add(tf.keras.layers.Dense(1) ) # , kernel_initializer=layer_init_2
+
         self.model.build()
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=alpha_w)
-
+        #self.optimizer = tf.keras.optimizers.RMSprop(learning_rate=alpha_w)
 
         # initialize model parameters
-        self.w = self.model.trainable_variables
-        for ix, grad in enumerate(self.w):
-            self.w[ix] = grad * 0
 
-    def call(self, state):
+        self.gradients = self.model.trainable_variables
+        for ix, grad in enumerate(self.gradients):
+            self.gradients[ix] = grad * 0
+        #print("self.model.trainable_variables: ", self.model.trainable_variables)
+
+    def call(self, state): # LFPR, c'est quoi ca?
         state = state[None]
-        return self.model(state)
+        state = tf.convert_to_tensor(state)
+        return tf.reshape(self.model(state), []) # LFPR: [] ici?
 
-    def update(self, state, reward):
-        compute_loss = tf.keras.losses.MeanSquaredError()
+    def update(self, state0, state1, r, done):
+        gamma = args.gamma
+        #compute_loss = tf.keras.losses.MeanSquaredError() #LFPR: J'ai enleve ça
 
-        state = state[None]
         with tf.GradientTape() as tape:
-            # forward pass
-            value = self(state)
-            loss = compute_loss(reward, value)
-        grads = tape.gradient(loss, self.model.trainable_variables)
+            tape.watch(self.model.trainable_variables)
+            if done:
+                v1 = 0      # terminal state
+            else:
+                v1 = self(state1)
+            v0 = self(state0)
+            target = r + gamma * v1
+            delta = target - v0
 
-        return grads
+            #loss = compute_loss([target], [v0]) # LFPR: J'ai enlevé ça
+            # loss = delta ** 2
+        #grads = tape.gradient(loss, self.model.trainable_variables) # LFPR: J'ai remplacé ça
+        grads = tape.gradient(v0, self.model.trainable_variables)
 
-    def apply_gradients(w):
-        self.optimizer.apply_gradients(zip(w, self.model.trainable_variables))
-        for idx, grad in enumerate(w):
-            w[idx] = grad * 0
+        for idx, grad in enumerate(grads):
+            self.gradients[idx] += grad * delta
 
+        return delta
 
-
+    def apply_gradients(self):
+        self.optimizer.apply_gradients(zip(self.gradients, self.model.trainable_variables))
+        # for idx, grad in enumerate(self.model.trainable_variables):
+        #     print('Sum w{}: {}'.format(idx, np.sum(np.abs(grad))))
+        for idx, grad in enumerate(self.gradients):
+            self.gradients[idx] = grad * 0
+        
 # #############################################################################
 #
 # Methods
@@ -207,15 +263,15 @@ def reinforce(alpha, seed=None):
     n_episodes = args.episodes
     update_every = args.update_every
     gamma = args.gamma
+    scores = []
 
     assert alpha > 0
     assert 0 <= gamma <= 1
 
-    env = gym.make(args.env)
+    env = gym.make(args.env).unwrapped
+    env.spec.max_episode_steps = args.max_steps
 
-    # sets optimizer and learning rate
-    optimizer = tf.keras.optimizers.Adam(learning_rate=alpha)
-    pi = policy(env, seed)
+    pi = policy(env, alpha, seed)
 
     for e in range(n_episodes):
         state = env.reset()
@@ -225,9 +281,14 @@ def reinforce(alpha, seed=None):
         while not done:
             action, grads = pi.choose_action(state)
             state, r, done, _ = env.step(action)
-            steps += 1
             if done: r -= 10    # makes training faster
-            episode.append([grads, r])
+            episode.append([grads, r]) #LFPR: r est toujours 1?
+            steps += 1
+
+             # update weights
+            pi.update(episode)
+            pi.apply_gradients()
+
 
         scores.append(steps)
 
@@ -235,14 +296,11 @@ def reinforce(alpha, seed=None):
         episode = np.array(episode)
         episode[:, 1] = discount_rewards(episode[:, 1], gamma)
 
-        for grads, r in episode:
-            for idx, grad in enumerate(grads):
-                pi.theta[idx] += grad * r
+        # # update weights
+        # pi.update(episode)
 
-        if e % update_every == 0:
-            optimizer.apply_gradients(zip(pi.theta, pi.model.trainable_variables))
-            for idx, grad in enumerate(pi.theta):
-                pi.theta[idx] = grad * 0
+        # if e % update_every == 0:
+        #     pi.apply_gradients()
 
         if e % 100 == 0:
             print("Episode  {}  Score  {}".format(e, np.mean(scores[-100:])))
@@ -250,38 +308,63 @@ def reinforce(alpha, seed=None):
 
 def actor_critic(alpha_t, alpha_w, seed=None):
 
+    global best_scores_per_hyperparams
+    if (alpha_t, alpha_w) in best_scores_per_hyperparams.keys():
+        best_scores_per_hyperparams[(alpha_t, alpha_w)].append([0])
+    else:
+        best_scores_per_hyperparams[(alpha_t, alpha_w)] = [0]
+
     n_episodes = args.episodes
     update_every = args.update_every
     gamma = args.gamma
+    scores = []
 
     assert 0 <= gamma <= 1
     assert alpha_t > 0
     assert alpha_w > 0
 
-    env = gym.make(args.env)
+    env = gym.make(args.env).unwrapped
+    env._max_episode_steps = args.max_steps
 
-    # sets optimizer and learning rate
-    actor_opt = tf.keras.optimizers.Adam(learning_rate=alpha_t)
-    actor = policy(env, seed)
-
-    critic = v_hat(alpha_w)
+    actor = policy(env, alpha_t, seed)
+    critic = v_hat(
+        alpha_w,
+        input_dim=actor.env.observation_space.shape[0]
+        )
 
     for e in range(n_episodes):
         state0 = env.reset()
         episode = []
         steps = 0
         done = False
-        I = 1
+        i = 1
+        #print("value of initial state (with w): ", critic(state0))
 
         while not done:
+            #print("value of state (with w) is {} at step {}".format(critic(state0), steps))
             action, grads = actor.choose_action(state0)
-            state1, R, done, _ = env.step(action)
+            state1, r, done, _ = env.step(action)
             steps += 1
-            delta = R + gamma * critic(state1, w) - critic(state0, w)
-            w += alpha_w * delta * z_w
-            theta += alpha_t * delta * z_t
-            I *= gamma
+            #if done: r -= 10    # makes training faster
+
+            delta = critic.update(state0, state1, r, done)
+            actor.update([[grads, i * delta]])
+
+            actor.apply_gradients()
+            critic.apply_gradients()
+            
+            i *= gamma #LFPR: J'avais enlevé enlevé ça
             state0 = state1
+
+        scores.append(steps)
+
+
+
+        if (e % 100 == 0 and e > 0) :
+            last_100_scores_mean = np.mean(scores[-100:])
+            print("Episode  {}  Score  {}".format(e, last_100_scores_mean))
+            if last_100_scores_mean > best_scores_per_hyperparams[(alpha_t, alpha_w)][-1]:
+                best_scores_per_hyperparams[(alpha_t, alpha_w)][-1] = last_100_scores_mean
 
 
 # #############################################################################
@@ -360,7 +443,35 @@ def main():
         print('Using saved data from: {}'.format(filename))
     else:
         # steps = runs(env, alphas, lambdas)
-        reinforce(alpha=0.01, seed=197710)
+        #reinforce(alpha=0.005)
+
+        """
+        actor_critic(alpha_t=0.1, alpha_w=0.0003)
+        actor_critic(alpha_t=0.2, alpha_w=0.0003)
+        actor_critic(alpha_t=0.3, alpha_w=0.0003)
+        actor_critic(alpha_t=0.1, alpha_w=0.0003)
+        actor_critic(alpha_t=0.1, alpha_w=0.0003)
+        """
+
+        #alphas_t = [0.4, 0.2]
+        #alphas_w = [0.003]
+        # alphas_t = [0.4,0.2,0.1,0.05,0.03] # petits alpha_t ne marchent pas
+        # alphas_w = [0.01,0.003,0.001,0.0003,0.0001, 0.00003] #  LFPR
+        # #actor_critic(alpha_t=0.005, alpha_w=0.0001) #LFPR:  actor_critic(alpha_t=0.005, alpha_w=0.0001)
+        # for alpha_t in alphas_t:
+        #     for alpha_w in alphas_w:
+        #         print("alpha_t = {} and alpha_w = {}".format(alpha_t, alpha_w))
+        #         actor_critic(alpha_t, alpha_w)
+        #         actor_critic(alpha_t, alpha_w)
+        #         actor_critic(alpha_t, alpha_w)
+        actor_critic(alpha_t=0.1, alpha_w=0.0003)
+
+        global best_scores_per_hyperparams
+        print(best_scores_per_hyperparams)
+
+
+
+
         # save([steps, args], 'steps')
 
     # plot3('Average steps', steps, alphas, lambdas)
